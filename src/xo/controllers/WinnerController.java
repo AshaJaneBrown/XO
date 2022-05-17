c0776d47bb97e79295383c23d5c3824f53c64d7f
AshaJaneBrown <ashajanebrown@gmail.com>
2022-05-17 17:11:02 +0300

    WinnerController fixed (issues discovered during unit testing)

--- src/xo/controllers/WinnerController.java
+++ src/xo/controllers/WinnerController.java
@@ -8,7 +8,57 @@ import java.awt.*;
 
 public class WinnerController {
 
-/*    public Figure getWinner(final Field field) {
+    public Figure getWinner(final Field field) {
+        try {
+            for (int i = 0; i < 3; i++) {
+                if (check(field, new Point(i, 0), p -> new Point(p.x, p.y + 1)))
+                    return field.getFigure(new Point(i, 0));
+            }
+
+            for (int i = 0; i < 3; i++) {
+                if (check(field, new Point(0, i), p -> new Point(p.x + 1, p.y)))
+                    return field.getFigure(new Point(0, i));
+            }
+
+            if (check(field, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1)))
+                return field.getFigure(new Point(0, 0));
+
+            if (check(field, new Point(0, 2), p -> new Point(p.x + 1, p.y - 1)))
+                return field.getFigure(new Point(1, 1));
+
+
+        } catch (final InvalidPointException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private boolean check(final Field field,
+                          final Point currentPoint,
+                          final IPointGenerator pointGenerator) {
+        final Figure currentFigure;
+        final Figure nextFigure;
+        final Point nextPoint = pointGenerator.next(currentPoint);
+
+        try {
+            currentFigure = field.getFigure(currentPoint);
+            if (currentFigure == null) return false;
+
+            nextFigure = field.getFigure(nextPoint);
+        } catch (InvalidPointException e) {
+            return true;
+        }
+
+        if (currentFigure != nextFigure) return false;
+
+        return check(field, nextPoint, pointGenerator);
+    }
+
+    private interface IPointGenerator {
+        Point next(final Point point);
+    }
+
+    /*    public Figure getWinner(final Field field) {
         try {
             for (int i = 0; i < 3; i++)
                 if (check(field, new Point(i, 0), new Point(i, 1), new Point(i, 2)))
@@ -44,53 +94,4 @@ public class WinnerController {
         }
         return false;
     }*/
-
-    public Figure getWinner(final Field field) {
-        try {
-            for (int i = 0; i < 3; i++) {
-                if (check(field, new Point(i, 0), p -> new Point(p.x, p.y + 1)))
-                    return field.getFigure(new Point(i, 0));
-            }
-
-            for (int i = 0; i < 3; i++) {
-                if (check(field, new Point(0, i), p -> new Point(p.x + 1, p.y)))
-                    return field.getFigure(new Point(i, 0));
-            }
-
-            if (check(field, new Point(0, 0), p -> new Point(p.x + 1, p.y + 1)))
-                return field.getFigure(new Point(0, 0));
-
-            if (check(field, new Point(0, 2), p -> new Point(p.x + 1, p.y - 1)))
-                return field.getFigure(new Point(1, 1));
-
-
-        } catch (InvalidPointException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private boolean check(final Field field,
-                          final Point currentPoint,
-                          final IPointGenerator pointGenerator) {
-        final Figure currentFigure;
-        final Figure nextFigure;
-        final Point nextPoint = pointGenerator.next(currentPoint);
-
-        try {
-            currentFigure = field.getFigure(currentPoint);
-            nextFigure = field.getFigure(nextPoint);
-        } catch (InvalidPointException e) {
-            return true;
-        }
-
-        if (currentFigure == null) return false;
-        if (currentFigure != nextFigure) return false;
-
-        return check(field, nextPoint, pointGenerator);
-    }
-
-    private interface IPointGenerator {
-        Point next(final Point point);
-    }
 }
